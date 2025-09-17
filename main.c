9bb80491f8c9ff86f169944218c839e13cabda33
Olive Prichard <70983271+ChillyCoyote273@users.noreply.github.com>
2025-09-17 15:47:27 -0400

    cleaning up

--- main.c
+++ main.c
@@ -1,20 +1,7 @@
 #include <stdio.h>
 
 int main(void) {
-    printf("Enter a number: ");
-    fflush(stdout);
-    unsigned int num;
-    scanf("%u", &num);
-    printf("Enter a digit: ");
-    fflush(stdout);
-    unsigned int digit;
-    scanf("%u", &digit);
-    unsigned int count = 0;
-    for (unsigned n = num; n; n /= 10) {
-        if (n % 10 == digit)
-            count++;
-    }
-    if (num == 0 && digit == 0) count = 1;
-    printf("The digit %u appears %u times in %u.\n", digit, count, num);
+    printf("Hello World!");
+
     return 0;
 }
