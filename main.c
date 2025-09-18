9b42bec8ea54c078d5deef4aca1805fa356440b6
Olive Prichard <70983271+ChillyCoyote273@users.noreply.github.com>
2025-09-18 19:08:38 -0400

    rational approximation algorithm

--- main.c
+++ main.c
@@ -1,7 +1,42 @@
 #include <stdio.h>
+#include <math.h>
 
 int main(void) {
-    printf("Hello World!");
+    double target;
+    int maxDen;
+    printf("Enter floating point value between 0 and 1: ");
+    fflush(stdout);
+    scanf("%lf", &target);
+    printf("Enter maximum denominator: ");
+    fflush(stdout);
+    scanf("%d", &maxDen);
+    printf("\n");
+
+    int a = 0, b = 1;
+    int c = 1, d = 1;
+    while (b + c <= maxDen) {
+        const double mediant = (double)(a + c) / (b + d);
+        if (mediant <= target) {
+            a += c;
+            b += d;
+            if (mediant == target) break;
+        }
+        else {
+            c += a;
+            d += b;
+        }
+    }
+    const double firstDiff = target - (double)a / b;
+    const double secondDiff = (double)c / b - target;
+    if (secondDiff < firstDiff || secondDiff == firstDiff && d < b) {
+        a = c;
+        b = d;
+    }
+
+    printf("Closest fraction is %d/%d\n", a, b);
+    printf("Fraction value is %lf\n", (double)a / b);
+    printf("Error = %e\n", fabs(target - (double)a / b));
+    fflush(stdout);
 
     return 0;
 }
